6bd9e1ee8f301739d655253bba65ab2bac85cd7d
unknown <abeeson@PC-CZC11982Y9.netbuilder.private>
2015-08-03 14:58:00 +0100

    Added gitignore

--- NBGardens/src/Main.java
+++ NBGardens/src/Main.java
@@ -1,5 +1,8 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
-public class Main {
+
+public class Main extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
