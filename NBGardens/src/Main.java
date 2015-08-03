f3b8db707173c21603a91704b55e5fad29e9217e
unknown <abeeson@PC-CZC11982Y9.netbuilder.private>
2015-08-03 15:35:40 +0100

    End of day 03/08/2015

--- NBGardens/src/Main.java
+++ NBGardens/src/Main.java
@@ -1,14 +1,41 @@
-import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 public class Main extends JFrame {
 
+	
+	
+	private static void initUI(){
+		JFrame wind = new JFrame("SimpleWindow");
+		JLabel header = new JLabel("ORDER LIST");
+		JButton b1 = new JButton("Button One");
+		JButton b2 = new JButton("Button Two");
+		
+		header.setBounds(10,0, 100, 50);
+		b1.setBounds(10, 100, 100, 50);
+	    b2.setBounds(120, 100, 100,50);
+		
+		wind.setSize(500, 500);
+		wind.setLayout(null);
+		wind.add(header);
+		wind.add(b1);
+		wind.add(b2);
+		
+		wind.setVisible(true);
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World");
-		
+		initUI();
+				
 	}
 
+	
+	
 }
