c9e259594865990de4eb62f1ce6b98f2df32f807
unknown <abeeson@PC-CZC11982Y9.netbuilder.private>
2015-08-03 14:51:19 +0100

    Second Commit

---
+++ NBGardens/src/Orderlist.java
@@ -0,0 +1,33 @@
+
+public class Orderlist {
+
+	
+	public void pickOrder(){
+			
+	}
+		
+	public void showOrdersWaiting(){
+		
+	}
+	
+	public void showPickedOrder(){
+		
+	}
+
+	public void addStock(){
+		
+	}
+	
+	void updateInventory(){
+		
+	}
+	
+	void getOrders(){
+		
+	}
+	
+	void storeInfo(){
+		
+	}
+	
+}
